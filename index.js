90c641238bb6f476264a1fe7cb0f9a13d1e28fb6
arvind <bonandrion@gmail.com>
2017-11-23 16:31:40 -0700

    - refactor structure - fixed validate data inside details page - added summary components

--- index.js
+++ index.js
@@ -3,13 +3,10 @@ import PropTypes from 'prop-types';
 import Link from 'react-router-dom/Link';
 import Switch from 'react-router-dom/Switch';
 import Route from 'react-router-dom/Route';
-import Application from './routes/application';
-import ExamplePage from './routes/example-page';
-import HomePage from './routes/homepage';
-import Settings from './routes/settings';
+import Main from './lib/Main';
+import Settings from './lib/Settings';
 
 class Vendors extends Component {
-  // static actionNames = ['stripesHome', 'usersSortByName'];
   static propTypes = {
     stripes: PropTypes.shape({
       connect: PropTypes.func.isRequired,
@@ -19,21 +16,22 @@ class Vendors extends Component {
     showSettings: PropTypes.bool,
   }
 
+  constructor(props, context) {
+    super(props, context);
+    this.connectedApp = props.stripes.connect(Main);
+  }
 
   render() {
     if (this.props.showSettings) {
       return <Settings {...this.props} />;
     }
-    // <Route path={`${this.props.match.path}`} component={Application} />
-    // <ul>
-    //   <li><Link to={`${this.props.match.path}/home`}>[home]</Link></li>
-    //   <li><Link to={`${this.props.match.path}/examples`}>[examples]</Link></li>
-    // </ul>
     return (
       <div>
         <Switch>
-          <Route path={`${this.props.match.path}/`} component={HomePage} />
-          <Route path={`${this.props.match.path}/examples`} exact component={ExamplePage} />
+          <Route
+            path={`${this.props.match.path}`}
+            render={props => <this.connectedApp {...this.props} />}
+          />
         </Switch>
       </div>
     );
