1185669e1a10b223f5c6d3a7f5d3ef1107fef5be
arvind <bonandrion@gmail.com>
2018-07-16 10:00:16 -0600

    Fixed agreement and contact people stripes import path

--- src/ContactPeople/ContactPeopleForm.js
+++ src/ContactPeople/ContactPeopleForm.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Row, Col, Button, TextField, TextArea, Select } from '@folio/stripes-components';
 import { Field, FieldArray } from 'redux-form';
-import { Row, Col } from '@folio/stripes-components/lib/LayoutGrid';
-import Button from '@folio/stripes-components/lib/Button';
-import TextField from '@folio/stripes-components/lib/TextField';
-import TextArea from '@folio/stripes-components/lib/TextArea';
-import Select from '@folio/stripes-components/lib/Select';
 import { Required } from '../Utils/Validate';
 import css from './ContactPeopleForm.css';
 
